2d2a8ae8fa9c5b60b278ed3ddbfcffd3bfbea5f0
anaima <Namilya@gmail.com>
2016-02-24 11:48:38 +0100

    First commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ User.delete_all
 User.create!(email: 'admin@admin.com', password:"password", password_confirmation:"password")
 
 # # Use these variable to  populate text zones
+
 # text1 = "Donec commodo lacus arcu, vitae iaculis nisi pharetra accumsan. Phasellus nibh ligula, efficitur non metus sit amet, accumsan condimentum magna. Ut non cursus purus. Vestibulum eu lobortis enim. Vivamus mattis dictum tellus et tempus. Maecenas massa nunc, varius at risus eget, euismod varius ipsum. Nam accumsan, est sit amet interdum ultricies, tellus erat semper orci, in posuere urna dolor vitae erat. Aenean vel faucibus tortor. Morbi semper vulputate tortor, nec vulputate leo mollis quis. Vestibulum sit amet tincidunt risus. Vivamus purus nisl, porttitor eget tempor a, gravida eget eros. Fusce eget augue finibus, tincidunt nulla eu, rutrum sapien. Mauris sed semper orci."
 
 # text2 = "Vestibulum vestibulum elit eget purus mollis iaculis. Donec sed purus ut justo rutrum vestibulum. Suspendisse vitae commodo nibh. Nam facilisis rutrum libero vitae cursus. Curabitur dui odio, ultricies maximus nunc nec, dignissim dignissim lacus. Proin sagittis hendrerit neque sit amet ornare. Fusce pretium facilisis rhoncus. In nibh lacus, semper sed bibendum et, vehicula id nunc. Nunc quis nibh massa. Proin dapibus vel quam eget ultricies. Duis neque libero, viverra et gravida et, lobortis placerat turpis. Etiam consequat leo porttitor accumsan venenatis. Fusce ut mauris ipsum."
@@ -21,5 +22,5 @@ User.create!(email: 'admin@admin.com', password:"password", password_confirmatio
 # text4 = "Ut non velit in eros efficitur placerat in et sem. Quisque vitae mollis turpis. Phasellus vestibulum sollicitudin bibendum. Nam aliquam diam ipsum, non commodo nisi semper ac. Aenean malesuada magna vitae tempor porttitor. Cras et ligula rutrum leo ultrices tempus. Maecenas interdum mauris leo, vitae interdum diam mattis a. Phasellus a egestas odio. Curabitur tincidunt non erat non porta. Donec imperdiet porta quam, sed luctus dolor fringilla sed. Maecenas nunc orci, dictum et turpis non, rhoncus lacinia risus. Sed posuere tincidunt mi, vel ultrices felis tincidunt non. Maecenas pretium auctor iaculis. Fusce ipsum erat, ornare id convallis lobortis, tempus nec velit. Sed tristique mi elit, ut sodales dui feugiat quis."
 
 ## exemple:
-                                                                        
-# Post.create(name: "title exemple",content: text4) 
+
+# Post.create(name: "title exemple",content: text4)
